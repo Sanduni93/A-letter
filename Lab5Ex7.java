4d122a19a099ee43f39c070859fa8e93d1d6d692
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-07-26 15:13:14 +0530

    vowel or constat

---
+++ Lab5Ex7.java
@@ -0,0 +1,26 @@
+class Lab5Ex7{
+	
+	public static void main(String args[]){
+		
+		char a ='w';
+		
+		switch(a){
+			
+			case 'a': case 'A':
+		    case 'e': case 'E':
+			case 'i': case 'I':
+			case 'o': case 'O':
+			case 'u': case 'U':
+
+			System.out.print("Vowel");
+			break;
+			
+			default:
+			System.out.print("Constant");
+			
+		}
+		
+		
+	}
+	
+}
